5026c55f55b0a8d55897c741b3c59b0014425394
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-02-17 21:59:08 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { ResipeDetailsComponent } from './recipes/resipe-details/resipe-details.component';
+import { RecipeItemComponent } from './recipesr/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    ResipeDetailsComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/resipe-details/resipe-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resipe-details',
+  templateUrl: './resipe-details.component.html',
+  styleUrls: ['./resipe-details.component.css']
+})
+export class ResipeDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
