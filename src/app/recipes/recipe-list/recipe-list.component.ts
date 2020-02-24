194e88d4c53dfed98889b3b29ff2a2cebdaed473
Pubudusandeepa <ppsandeepa12@gmail.com>
2020-02-24 14:46:25 +0530

    Add recipie item in display

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes = [
+    new Recipe('A test Recipe',
+    'Tset ',
+     'https://www.freepik.com/free-photo/colorful-healthy-unhealthy-food-white-textured-background_2955602.htm' )
+  ];
   constructor() { }
 
   ngOnInit(): void {
